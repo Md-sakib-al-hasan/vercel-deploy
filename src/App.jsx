e8d436fece8387d17d0909ff617e0483bd45d112
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2024-10-29 14:06:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState([])
+
+  useEffect(() => {
+    fetch( import.meta.env.VITE_API_URL)
+    .then(res => res.json())
+    .then(data => setCount(data))
+},[])
+
+  return (
+
+    <>
+      <ul>
+           {
+            count?.map(item => <li key={item.id}>{item.name}</li>)
+           } 
+      </ul>
+        
+    </>
+  )
+}
+
+export default App
